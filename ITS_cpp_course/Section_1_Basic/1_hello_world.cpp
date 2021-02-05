c9c4d0ecca9fc1781ab3e9ebe338d7f9566159a9
Muhammad Luqman <noshluk2@gmail.com>
2021-02-05 11:50:56 +0500

    Add files via upload

---
+++ ITS_cpp_course/Section_1_Basic/1_hello_world.cpp
@@ -0,0 +1,21 @@
+
+/*
+    - Purpose : 
+    Writing first code
+    - Understand :
+            library including
+            main function
+            print format specifier
+            returning '0'
+
+
+
+    Written by Muhammad Luqman
+
+
+*/
+#include <cstdio>
+int main(){
+printf("hello luqman");
+return 0;
+}
---
+++ ITS_cpp_course/Section_1_Basic/2_variables.cpp
@@ -0,0 +1,76 @@
+
+/*
+    - Purpose : 
+    Writing first code
+    - Understand :
+            library including
+            main function
+            print format specifier
+            returning '0'
+
+
+
+    Written by Muhammad Luqman
+
+
+*/
+#include <cstdio>
+
+// using printf because we want to give discription to varibales
+int main(){
+    
+    
+    int integers= 10;
+    float number_2=10.2;
+    string greeting = "Hello";
+    char house[] = "a house of gold."; 
+    //formate specifier : https://www.cplusplus.com/reference/cstdio/printf/
+    printf("integer %d\n ", house);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
